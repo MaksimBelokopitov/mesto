64fc82c9bd8ed83200516b96135815829360850e
Maksim Belokopitov <alerio-zi@yandex.ru>
2023-07-17 20:57:20 +0300

    mesto/card-js

--- script/index.js
+++ script/index.js
@@ -1,6 +1,49 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+  function createInitialCards(arr) {
+    const mestoList = document.querySelector('.mesto__list');
+    const mestoTemplate = document.querySelector('#mesto-item').content;
+    
+    for (let i = 0; i < arr.length; i++){
+        const mestoElement = mestoTemplate.querySelector('.mesto__item').cloneNode(true);
+        const mestoPicture = mestoElement.querySelector('.mesto__image');
+        mestoPicture.alt = arr[i].name;
+        mestoPicture.src = arr[i].link;
+        const mestoName = mestoElement.querySelector('.mesto__title');
+        mestoName.textContent = arr[i].name;
+        mestoList.append(mestoElement);  
+    }
+  }
 
 document.addEventListener('DOMContentLoaded', function(){
    
+    createInitialCards(initialCards)
+
     const editButton = document.querySelector('.profile__edit-button');
     const addButton = document.querySelector('.profile__add-button');
     const profileWindow = document.querySelector('.popup_type_profile');
