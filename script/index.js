9adccf13123bba2dda8d3873392bf90cdcbda2e1
Maksim Belokopitov <alerio-zi@yandex.ru>
2023-07-17 21:32:24 +0300

    popup/add/card

--- script/index.js
+++ script/index.js
@@ -25,37 +25,36 @@ const initialCards = [
     }
   ]; 
 
-  function createInitialCards(arr) {
-    const mestoList = document.querySelector('.mesto__list');
-    const mestoTemplate = document.querySelector('#mesto-item').content;
-    
-    for (let i = 0; i < arr.length; i++){
-        const mestoElement = mestoTemplate.querySelector('.mesto__item').cloneNode(true);
-        const mestoPicture = mestoElement.querySelector('.mesto__image');
-        mestoPicture.alt = arr[i].name;
-        mestoPicture.src = arr[i].link;
-        const mestoName = mestoElement.querySelector('.mesto__title');
-        mestoName.textContent = arr[i].name;
-        mestoList.append(mestoElement);  
-    }
-  }
 
 document.addEventListener('DOMContentLoaded', function(){
+
+    //Создание карточек//
+
+    function createInitialCards(arr) {
+        const mestoList = document.querySelector('.mesto__list');
+        const mestoTemplate = document.querySelector('#mesto-item').content;
+        
+        for (let i = 0; i < arr.length; i++){
+            const mestoElement = mestoTemplate.querySelector('.mesto__item').cloneNode(true);
+            const mestoPicture = mestoElement.querySelector('.mesto__image');
+            mestoPicture.alt = arr[i].name;
+            mestoPicture.src = arr[i].link;
+            const mestoName = mestoElement.querySelector('.mesto__title');
+            mestoName.textContent = arr[i].name;
+            mestoList.append(mestoElement);  
+        }
+      }
    
-    createInitialCards(initialCards)
+    createInitialCards(initialCards);
+
+    //Открытие попапов//
 
     const editButton = document.querySelector('.profile__edit-button');
     const addButton = document.querySelector('.profile__add-button');
     const profileWindow = document.querySelector('.popup_type_profile');
     const mestoWindow = document.querySelector('.popup_type_mesto');
-    const profileCloseButton = document.querySelector('.popup__button-close_type_profile');
-    const mestoCloseButton = document.querySelector('.popup__button-close_type_mesto');
     const userName = document.querySelector('.profile__name');
     const userJob = document.querySelector('.profile__job');
-    const profileFormElement = document.querySelector('.popup__form_type_profile');
-    const mestoFormElement = document.querySelector('.popup__form_type_mesto');
-    const nameInput = document.querySelector('.popup__input_type_user-name')
-    const jobInput = document.querySelector('.popup__input_type_user-job')
 
     editButton.addEventListener('click', () => {
         profileWindow.classList.add('popup_opened');
@@ -67,6 +66,11 @@ document.addEventListener('DOMContentLoaded', function(){
         mestoWindow.classList.add('popup_opened');
     });
 
+    // Закрытие попапов //
+
+    const profileCloseButton = document.querySelector('.popup__button-close_type_profile');
+    const mestoCloseButton = document.querySelector('.popup__button-close_type_mesto');
+
     function closeProfile() {
         profileWindow.classList.remove('popup_opened');
     };
@@ -74,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function(){
     function closeMesto() {
         mestoWindow.classList.remove('popup_opened');
     };
-
+    
     profileCloseButton.addEventListener('click', () => {
         closeProfile();
     });
@@ -83,6 +87,17 @@ document.addEventListener('DOMContentLoaded', function(){
         closeMesto();
     });
 
+    // Работа форм в попапах
+
+        // Редактирование профиля//
+
+    const profileFormElement = document.querySelector('.popup__form_type_profile');
+    const mestoFormElement = document.querySelector('.popup__form_type_mesto');
+    const nameInput = document.querySelector('.popup__input_type_user-name')
+    const jobInput = document.querySelector('.popup__input_type_user-job');
+    const cardNameInput = document.querySelector('.popup__input_type_mesto-name');
+    const cardImageInput = document.querySelector('.popup__input_type_mesto-image');
+
     function handleProfileFormSubmit (evt) {
         evt.preventDefault(); 
         userName.textContent = nameInput.value;
@@ -90,6 +105,34 @@ document.addEventListener('DOMContentLoaded', function(){
         closeProfile();
     };  
 
-    profileFormElement.addEventListener('submit', handleProfileFormSubmit)
+    profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+    //Создание новой карточки//
+   
+    function createNewCard() {
+        const mestoList = document.querySelector('.mesto__list');
+        const mestoTemplate = document.querySelector('#mesto-item').content;
+        const mestoElement = mestoTemplate.querySelector('.mesto__item').cloneNode(true);
+        const mestoPicture = mestoElement.querySelector('.mesto__image');
+        mestoPicture.alt = cardNameInput.value;
+        mestoPicture.src = cardImageInput.value;
+        const mestoName = mestoElement.querySelector('.mesto__title');
+        mestoName.textContent = cardNameInput.value;
+        mestoList.prepend(mestoElement);  
+      };
+
+      function handleMestoFormSubmit(evt) {
+        evt.preventDefault();
+        createNewCard();
+        closeMesto();
+      };
+
+      mestoFormElement.addEventListener('submit', handleMestoFormSubmit);
+
+      
+
+   
+
+ 
 });
 
